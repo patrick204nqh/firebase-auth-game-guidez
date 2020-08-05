27d3e5c3eea4242825e4cf82ae1d792f8ba13166
patrick <patrick204nqh@gmail.com>
2020-08-05 18:10:16 +0700

    dynamic data form firestore

--- scripts/auth.js
+++ scripts/auth.js
@@ -1,14 +1,12 @@
-// get data
-db.collection('guides').get().then((snapshot) => {
-  setupGuides(snapshot.docs);
-})
-
 // listen for auth status changes
 auth.onAuthStateChanged(user => {
   if (user) {
-    console.log('user logged in: ', user)
+    // get data
+    db.collection('guides').get().then((snapshot) => {
+      setupGuides(snapshot.docs);
+    })
   } else {
-    console.log('user logged out')
+    setupGuides([])
   }
 })
 
