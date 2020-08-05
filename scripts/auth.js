5ba74f039dd99703eb751d6fcec15a2192534e9c
patrick <patrick204nqh@gmail.com>
2020-08-05 13:28:17 +0700

    tracking auth status

--- scripts/auth.js
+++ scripts/auth.js
@@ -1,3 +1,12 @@
+// listen for auth status changes
+auth.onAuthStateChanged(user => {
+  if (user) {
+    console.log('user logged in: ', user)
+  } else {
+    console.log('user logged out')
+  }
+})
+
 // signup
 const signupForm = document.querySelector('#signup-form');
 signupForm.addEventListener('submit', (e) => {
@@ -20,9 +29,7 @@ signupForm.addEventListener('submit', (e) => {
 const logout = document.querySelector('#logout');
 logout.addEventListener('click', (e) => {
   e.preventDefault();
-  auth.signOut().then(() => {
-    console.log('user signed out')
-  });
+  auth.signOut()
 });
 
 // login
@@ -35,7 +42,6 @@ loginForm.addEventListener('submit', (e) => {
   const password = loginForm['login-password'].value;
 
   auth.signInWithEmailAndPassword(email, password).then(cred => {
-    console.log(cred.user);
     // close the login modal and reset the form
     const modal = document.querySelector('#modal-login');
     M.Modal.getInstance(modal).close();
